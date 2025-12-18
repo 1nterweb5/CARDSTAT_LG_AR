6c2a5ed4373f2bb3e4edb87bce6e9cc883479dca
unknown <tophatlucas@gmail.com>
2025-12-18 19:36:48 +0100

    continuo

--- cardstat/main.cpp
+++ cardstat/main.cpp
@@ -31,20 +31,22 @@ int main()
     return 0;
 }
 void sorteggio (int a) {
-    char seme, rosso = 'R', nero = 'N', figura;
-    int valore,colore;
+    char seme, figura, colore;
+    int valore;
     srand(time(NULL));
+
+    for (int contatore=a; contatore>0; contatore--){
     for (int contatore=a; contatore>0; contatore--){
        valore = (rand () % 13);
+
     }
 
      for (int contatore=a; contatore>0; contatore--){//da otimizzare: trovare un modo per far si che l'azione rand sceglie automaticamente tra R (rosso) o N (nero) e togliere l'assegnazione con l'if
-       colore = (rand () % 2);
-       if (colore== 1){
-        colore = rosso;
-       } else {
-        colore = nero;
-       }
+       colore = (rand () % "N", "R");
+     }
+
+     cout << valore << colore << endl;
+
      }
 
 
@@ -55,7 +57,7 @@ void sorteggio (int a) {
 
 
 
-       cout << valore << endl;
+
 
 
 
