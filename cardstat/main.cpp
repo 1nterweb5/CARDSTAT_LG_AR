3d30f5353d69c3c697bc002f5b3a34e127559b47
Andrea Ricci <aricci1509@gmail.com>
2025-12-18 19:23:57 +0100

    fallo te

--- cardstat/main.cpp
+++ cardstat/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-
+void sorteggio (int a);
 int main()
 {
     int tipoMazzo, numSorteggi;
@@ -13,10 +13,10 @@ int main()
     do {
         cout << "Inserire il tipo di mazzo (40 o 52)" << endl;
         cin >> tipoMazzo;
-        if (tipoMazzo != 40 || tipoMazzo != 52) {
+        if (tipoMazzo != 40 && tipoMazzo != 52) {
             cout << "Non e' stato inserito un valore corretto" << endl;
         }
-    } while (tipoMazzo != 40 || tipoMazzo != 52);
+    } while (tipoMazzo != 40 && tipoMazzo != 52);
 
     do {
         cout << "inserire il numero di sorteggi da eseguire" << endl;
@@ -25,6 +25,38 @@ int main()
             cout << "Non e' stato inserito un valore corretto" << endl;
         }
     } while (numSorteggi < 2);
+    cout << endl;
+    sorteggio(numSorteggi);
 
     return 0;
+}
+void sorteggio (int a) {
+    char seme, rosso = 'R', nero = 'N', figura;
+    int valore,colore;
+    srand(time(NULL));
+    for (int contatore=a; contatore>0; contatore--){
+       valore = (rand () % 13);
+    }
+
+     for (int contatore=a; contatore>0; contatore--){//da otimizzare: trovare un modo per far si che l'azione rand sceglie automaticamente tra R (rosso) o N (nero) e togliere l'assegnazione con l'if
+       colore = (rand () % 2);
+       if (colore== 1){
+        colore = rosso;
+       } else {
+        colore = nero;
+       }
+     }
+
+
+
+
+
+
+
+
+
+       cout << valore << endl;
+
+
+
 }
