255b7dccd490b20b2b59d2107bd61c75d5b6b200
unknown <tophatlucas@gmail.com>
2025-12-19 20:01:53 +0100

    fine sorteggio

--- cardstat/main.cpp
+++ cardstat/main.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 
-void sorteggio (int a);
+void sorteggio (int a, int valMax);
 int main()
 {
-    int tipoMazzo, numSorteggi;
+    int tipoMazzo, numSorteggi, valoreMassimo;
 
     do {
         cout << "Inserire il tipo di mazzo (40 o 52)" << endl;
@@ -18,6 +18,12 @@ int main()
         }
     } while (tipoMazzo != 40 && tipoMazzo != 52);
 
+    if (tipoMazzo == 40) {
+        valoreMassimo = 10;
+    } else {
+        valoreMassimo = 13;
+    }
+
     do {
         cout << "inserire il numero di sorteggi da eseguire" << endl;
         cin >> numSorteggi;
@@ -26,39 +32,61 @@ int main()
         }
     } while (numSorteggi < 2);
     cout << endl;
-    sorteggio(numSorteggi);
+    sorteggio(numSorteggi, valoreMassimo);
 
     return 0;
 }
-void sorteggio (int a) {
+void sorteggio (int a, int valMax) {
     char seme, figura, colore;
-    int valore;
+    int valore, sceltaSeme, sceltaColore;
+
     srand(time(NULL));
 
     for (int contatore=a; contatore>0; contatore--){
-    for (int contatore=a; contatore>0; contatore--){
-       valore = (rand () % 13);
+        for (int contatore=a; contatore>0; contatore--){
+           valore = 1 + (rand () % valMax);
+           if (valore >= valMax - 3) {
+                if (valore == (valMax - 2)) {
+                    figura = 'F';
+                } else if (valore == (valMax - 1)) {
+                    figura = 'D';
+                } else if (valore == valMax) {
+                    figura = 'R';
+                }
 
-    }
+           } else {
+                figura = NULL;
+           }
 
-     for (int contatore=a; contatore>0; contatore--){//da otimizzare: trovare un modo per far si che l'azione rand sceglie automaticamente tra R (rosso) o N (nero) e togliere l'assegnazione con l'if
-       colore = (rand () % "N", "R");
-     }
+        }
 
-     cout << valore << colore << endl;
+        for (int contatore=a; contatore>0; contatore--){
+           sceltaColore = (rand () % 2);
+           if (sceltaColore == 0) {
+                colore = 'r';
+           } else {
+                colore = 'n';
+           }
+        }
 
-     }
+        for (int contatore=a; contatore>0; contatore--){
+           sceltaSeme = (rand () % 4);
+           switch (sceltaSeme) {
+               case 0:
+                   seme = 'C';
+                   break;
+               case 1:
+                   seme = 'Q';
+                   break;
+               case 2:
+                   seme = 'F';
+                   break;
+               case 3:
+                   seme = 'P';
+                   break;
+           }
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
+        cout << seme << valore << colore << figura << endl;
+        }
 }
