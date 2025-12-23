acd06335db295a47ffe7d1da027bdd1fae37ff82
unknown <tophatlucas@gmail.com>
2025-12-23 16:27:12 +0100

    FINE!!!!!!!

--- cardstat/main.cpp
+++ cardstat/main.cpp
@@ -7,11 +7,20 @@ using namespace std;
 
 void sorteggio (int sorteggi, int valMax);
 
-void controlloCoppieInt (int valoreNuovo, int valoreVecchio, int sorteggi, int contatoreSorteggi);
+void calcoloProbabilita (double numCoppie, int numSorteggi);
 
-int main()
-{
+int main() {
     int tipoMazzo, numSorteggi, valoreMassimo;
+
+    cout << "--------------------ALGORITMO CALCOLO DELLA PROBABILITA'--------------------" << endl << endl << endl
+         << "Questo e' un algoritmo che, una volta specificato il tipo di mazzo e il numero di estrazioni, "
+         << "peschera' casualmente delle carte che presenteranno ognuna 4 attributi: " << endl << endl
+         << "-Un valore numerico" << endl
+         << "-Una figura (nel caso il valore numerico corrisponda al valore di una figura)" << endl
+         << "-Un seme" << endl
+         << "-Un colore (in base al seme)" << endl << endl
+         << "Una volta mostrate tutte le estrazioni, il programma mostrera' il numero di coppie per ogni attributo, "
+         << "e la probabilita' di aver pescato quelle carte." << endl << endl;
 
     do {
         cout << "Inserire il tipo di mazzo (40 o 52)" << endl;
@@ -41,21 +50,20 @@ int main()
 }
 void sorteggio (int sorteggi, int valMax) {
     char seme1, figura1, colore1, seme2, figura2, colore2;
-    int valore1, valore2, sceltaSeme, sceltaColore;
+    int valore1, valore2, sceltaSeme, coppieVal = 0, coppieSeme = 0, coppieFigura= 0, coppieColore= 0;
 
     srand(time(NULL));
 
-    for (int contatoreSort = sorteggi; contatoreSort>0; contatoreSort--){
+    for (int contatoreSort = sorteggi; contatoreSort>0; contatoreSort--) {
 
         if (contatoreSort <= sorteggi - 1) {
             valore2 = valore1;
-            figura2 = figura2;
+            figura2 = figura1;
         }
 
-
-        for (int contatore = sorteggi; contatore>0; contatore--){
-           valore1 = 1 + (rand () % valMax);
-           if (valore1 >= valMax - 3) {
+        for (int contatore = sorteggi; contatore>0; contatore--) {
+            valore1 = 1 + (rand () % valMax);
+            if (valore1 >= valMax - 3) {
                 if (valore1 == (valMax - 2)) {
                     figura1 = 'F';
                 } else if (valore1 == (valMax - 1)) {
@@ -64,60 +72,70 @@ void sorteggio (int sorteggi, int valMax) {
                     figura1 = 'R';
                 }
 
-           } else {
-                figura1 = NULL;
-           }
-         if (contatoreSort <= sorteggi - 1) {
-                controlloCoppieInt(valore1, valore2, sorteggi, contatoreSort);
-         }
-
+            } else {
+                figura1 = 0;
+            }
+        }
+        if (contatoreSort <= sorteggi - 1) {
+            if (valore1 == valore2) {
+                coppieVal++;
+            }
+            if (figura1 == figura2 && figura1 != 0) {
+                coppieFigura++;
+            }
         }
 
         if (contatoreSort <= sorteggi - 1) {
             colore2 = colore1;
-        }
-
-        if (contatoreSort <= sorteggi - 1) {
             seme2 = seme1;
         }
 
-        for (int contatore = sorteggi; contatore>0; contatore--){
-           sceltaSeme = (rand () % 4);
-           switch (sceltaSeme) {
-               case 0:
-                   seme1 = 'C';
-                   colore1 = 'r';
-                   break;
-               case 1:
-                   seme1 = 'Q';
-                   colore1 = 'r';
-                   break;
-               case 2:
-                   seme1 = 'F';
-                   colore1 = 'n';
-                   break;
-               case 3:
-                   seme1 = 'P';
-                   colore1 = 'n';
-                   break;
-           }
+        for (int contatore = sorteggi; contatore>0; contatore--) {
+            sceltaSeme = (rand () % 4);
+            switch (sceltaSeme) {
+            case 0:
+                seme1 = 'C';
+                colore1 = 'r';
+                break;
+            case 1:
+                seme1 = 'Q';
+                colore1 = 'r';
+                break;
+            case 2:
+                seme1 = 'F';
+                colore1 = 'n';
+                break;
+            case 3:
+                seme1 = 'P';
+                colore1 = 'n';
+                break;
+            }
         }
 
-        cout << seme1 << valore1 << colore1 << figura1 << endl;
+        if (contatoreSort <= sorteggi - 1) {
+            if (seme1 == seme2) {
+                coppieSeme++;
+            }
+            if (colore1 == colore2) {
+                coppieColore++;
+            }
         }
+
+        cout << seme1 << valore1 << figura1 << endl;
+    }
+    cout << "Le coppie sulla base del valore numerico sono ";
+    calcoloProbabilita(coppieVal, sorteggi);
+    cout << "Le coppie sulla base delle figure sono ";
+    calcoloProbabilita(coppieFigura, sorteggi);
+    cout << "Le coppie sulla base del seme sono ";
+    calcoloProbabilita(coppieSeme, sorteggi);
+    cout << "Le coppie sulla base del colore sono ";
+    calcoloProbabilita(coppieColore, sorteggi);
 }
 
-void controlloCoppieInt (int valoreNuovo, int valoreVecchio, int sorteggi, int contatoreSorteggi) {
-    int coppie = 0;
+void calcoloProbabilita (double numCoppie, int numSorteggi) {
     double probabilita;
 
-    if (contatoreSorteggi > 0) {
-        if (valoreNuovo == valoreVecchio) {
-            coppie++;
-        }
-    } else {
-        probabilita = coppie / sorteggi;
-        cout << "ci sono state" << coppie << "sulla base del valore numerico, "
-        << "e la probabilita' di aver pescato queste coppie è di" << probabilita << endl;
-    }
+    probabilita = (numCoppie / numSorteggi) * 100;
+    cout << numCoppie << " e la probabilita' di aver pescato queste coppie e' di " << probabilita << "%" << endl;
 }
